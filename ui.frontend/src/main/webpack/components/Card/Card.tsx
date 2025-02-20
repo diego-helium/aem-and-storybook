c892f0de73e63aecf20c416cba8ba2b6ea145fab
Diego Azevedo <diego@iamhelium.com.au>
2025-02-20 16:38:15 +1100

    card with no Image

--- ui.frontend/src/main/webpack/components/Card/Card.tsx
+++ ui.frontend/src/main/webpack/components/Card/Card.tsx
@@ -10,13 +10,14 @@ export interface CardProps {
   theme?: "light" | "dark";
 }
 
-import nahidPhoto from "../../../../stories/assets/nahid_photo.jpg";
-
 export const Card = ({ title, roles, description, image, theme }: CardProps) => {
   return (
     <div className={`cmp-card ${theme}`}>
       <div className="cmp-card__content">
-        {image && <img src={nahidPhoto} alt={title} className="cmp-card__image" />}
+        <div className="cmp-card__image-container">
+          <div className="cmp-card__image-skeleton" />
+          {image && <img src={image} alt={title} className="cmp-card__image" />}
+        </div>
         <div className="cmp-card__header">
           <h2 className="cmp-card__name">{title}</h2>
           <p className="cmp-card__roles">{roles?.join(", ")}</p>
