401cc917f964f4b1f5b6af60de3e5445a84e0fd4
Diego Azevedo <diego@iamhelium.com.au>
2025-02-20 16:38:15 +1100

    card with no Image

--- ui.frontend/src/main/webpack/components/Card/Card.stories.ts
+++ ui.frontend/src/main/webpack/components/Card/Card.stories.ts
@@ -4,6 +4,10 @@ import { Card } from './Card';
 
 import { parameters } from '../../../../../.storybook/stories_config';
 const figmaUrl = 'https://www.figma.com/design/wTzU9RR9oKfNvyet4LG9It/Cards-(Community)?node-id=1-773';
+const figmaUrlLight = 'https://www.figma.com/design/wTzU9RR9oKfNvyet4LG9It/Cards-(Community)?node-id=1-766&t=VU5P5HyJGAiIr7Fe-4';
+
+
+import nahidPhoto from "../../../../stories/assets/nahid_photo.jpg";
 
 
 const meta = {
@@ -27,15 +31,29 @@ export const Primary: Story = {
   args: {
     title: 'Nahid Hasan',
     roles: ['UX/UI Designer'],
-    image: 'https://picsum.photos/id/237/200/200',
+    image: nahidPhoto,
     description: ['UI is the saddle, the stirrups, & the reins. UX is the feeling you get being able to ride the horse.'
     ],
   },
 };
 
-export const Dark: Story = {
+export const Light: Story = {
   args: {
     ...Primary.args,
     theme: 'dark',
+  },
+  parameters: {
+    ...meta.parameters,
+    design: parameters({ url: figmaUrlLight }).design,
+  },
+};
+
+export const NoImage: Story = {
+  args: {
+    ...Primary.args,
+    title: 'No Image',
+    image: undefined,
+    roles: ['Ex-rich'],
+    description: ['Never worked a day in my life. Can you believe that? Need to get a job, help!'],
   },
 };
