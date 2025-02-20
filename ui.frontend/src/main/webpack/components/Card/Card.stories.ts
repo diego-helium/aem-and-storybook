995ae66a01640438d303dd8890dbd531e9870a35
Diego Azevedo <diego@iamhelium.com.au>
2025-02-20 18:30:29 +1100

    horizontal card

--- ui.frontend/src/main/webpack/components/Card/Card.stories.ts
+++ ui.frontend/src/main/webpack/components/Card/Card.stories.ts
@@ -6,9 +6,7 @@ import { parameters } from '../../../../../.storybook/stories_config';
 const figmaUrl = 'https://www.figma.com/design/wTzU9RR9oKfNvyet4LG9It/Cards-(Community)?node-id=1-773';
 const figmaUrlLight = 'https://www.figma.com/design/wTzU9RR9oKfNvyet4LG9It/Cards-(Community)?node-id=1-766&t=VU5P5HyJGAiIr7Fe-4';
 
-
 import nahidPhoto from "../../../../stories/assets/nahid_photo.jpg";
-
 
 const meta = {
   title: 'Components/Card',
@@ -29,6 +27,8 @@ type Story = StoryObj<typeof meta>;
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary: Story = {
   args: {
+    variant: 'vertical',
+    theme: 'light',
     title: 'Nahid Hasan',
     roles: ['UX/UI Designer'],
     image: nahidPhoto,
@@ -55,5 +55,12 @@ export const NoImage: Story = {
     image: undefined,
     roles: ['Ex-rich'],
     description: ['Never worked a day in my life. Can you believe that? Need to get a job, help!'],
+  },
+};
+
+export const Horizontal: Story = {
+  args: {
+    ...Primary.args,
+    variant: 'horizontal',
   },
 };
