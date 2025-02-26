fddf2342c9a316cc9872da1fb67b6be5bc2bdeb1
Diego Azevedo <diego@iamhelium.com.au>
2025-02-26 11:43:52 +1100

    Update ESLint configuration to include Storybook recommended rules

--- ui.frontend/.eslintrc.js
+++ ui.frontend/.eslintrc.js
@@ -1,7 +1,9 @@
 module.exports = {
   parser: "@typescript-eslint/parser", // Specifies the ESLint parser
   extends: [
-    "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    "plugin:@typescript-eslint/recommended",
+    "plugin:storybook/recommended",
+    // Uses the recommended rules from the @typescript-eslint/eslint-plugin
   ],
   parserOptions: {
     ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
