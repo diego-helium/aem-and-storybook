999dcc6f6a87f883f88ca6dd6965707e68a5699c
Diego Azevedo <diego@iamhelium.com.au>
2025-03-03 11:16:59 +1100

    Update Cypress configuration to simplify plugin setup

--- ui.frontend/cypress.config.ts
+++ ui.frontend/cypress.config.ts
@@ -5,10 +5,7 @@ import getCompareSnapshotsPlugin from 'cypress-image-diff-js/plugin';
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      return getCompareSnapshotsPlugin(on, {
-        ...config,
-        projectRoot: "./cypress/visual-test",
-      });
+      return getCompareSnapshotsPlugin(on, config);
     },
   },
 });
