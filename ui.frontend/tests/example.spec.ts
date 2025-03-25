e1d0748fa6fa25fd9105ae99ed2f2b2b2fd8d827
Diego Azevedo <diego@iamhelium.com.au>
2025-03-25 12:54:23 +1100

    Refactor visual tests and update screenshots
    - Updated the test to use a dynamic URL for the homepage visual test.
    - Changed the expected screenshot filename from 'homepage-lfs.png' to 'homepage.png'.
    - Added new baseline screenshots for Chromium, Firefox, and WebKit.
    - Removed outdated LFS screenshots for previous test runs.

--- ui.frontend/tests/example.spec.ts
+++ ui.frontend/tests/example.spec.ts
@@ -1,7 +1,13 @@
 import { test, expect } from '@playwright/test';
 
+const pageUrl = 'https://diego-helium.github.io/aem-and-storybook/iframe.html?viewMode=story&id=';
+
+test.beforeEach(async ({ page }) => {
+  await page.setViewportSize({ width: 1920, height: 1080 });
+});
+
 test('homepage visual test', async ({ page }) => {
-  await page.goto('https://diego-helium.github.io/aem-and-storybook/?path=/story/components-card--vertical');
+  await page.goto(`${pageUrl}components-card--vertical`);
   await page.waitForTimeout(2000);
-  await expect(page).toHaveScreenshot('homepage-lfs.png');
+  await expect(page).toHaveScreenshot('homepage.png');
 });
